0d88f9b6087d2d36a3a8713452a7f8c0af30d516
toma1128 <kirima--idea--@au.com>
2025-12-10 12:19:52 +0900

    [update]ルーティングができるように更新

--- src/infrastructure/server/index.ts
+++ src/infrastructure/server/index.ts
@@ -1,10 +1,11 @@
 import mysql from 'mysql2/promise';
-import { drizzle, DrizzleMySQL2Database } from 'drizzle-orm/mysql2';
+import { drizzle, MySql2Database } from 'drizzle-orm/mysql2';
 import { migrate } from 'drizzle-orm/mysql2/migrator';
-//import * as schema from './schema';
+import * as schema from '../db/schema';
 import { env } from '../../config/env';
+import { createApp } from './app';
 
-export async function initializeDatabase(): Promise<DrizzleMySQL2Database<typeof schema>> {
+export async function initializeDatabase(): Promise<MySql2Database<typeof schema>> {
     const { HOST, PORT, USER, PASSWORD, NAME } = env.DB;
 
     const adminConnection = await mysql.createConnection({
@@ -38,4 +39,14 @@ export async function initializeDatabase(): Promise<DrizzleMySQL2Database<typeof
     console.log('✅ Migrations completed.');
 
     return db;
+}
+
+const db = await initializeDatabase();
+
+const app = createApp(db)
+
+export default {
+    port: env.AUTH_PORT,
+    host: env.AUTH_HOST,
+    fetch: app.fetch
 }
